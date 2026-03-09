53bfb94251359561cf20cf1de781b48c3cfa6e43
Pallavi Hegde <pallavighegde16@gmail.com>
2026-03-09 14:29:43 +0530

    feat: updated api methods

--- backend/main.go
+++ backend/main.go
@@ -52,8 +52,8 @@ func main() {
 	createTable()
 
 	// Register HTTP handlers
-	http.HandleFunc("/api/v1/events", corsMiddleware(handleEvents))
-	http.HandleFunc("/api/v1/events/", corsMiddleware(handleEvent))
+	http.HandleFunc("/api/v1/events", corsMiddleware(eventsHandler))
+	http.HandleFunc("/api/v1/events/", corsMiddleware(eventHandler))
 
 	log.Println("Server running on http://localhost:8081")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
@@ -61,6 +61,181 @@ func main() {
 	}
 }
 
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		createEvent(w, r)
+		return
+	}
+
+	if r.Method == http.MethodGet {
+		getAllEvents(w, r)
+		return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+func eventHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+		getEvent(w, r)
+		return
+	}
+
+	if r.Method == http.MethodDelete {
+		deleteEvent(w, r)
+		return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+func createEvent(w http.ResponseWriter, r *http.Request) {
+
+	var input Event
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	result, err := db.Exec(`
+	INSERT INTO events
+	(title, company, description, start_time, end_time, status, venue, organizers, booking_link, source, verified, is_public)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		input.Title,
+		input.Company,
+		input.Description,
+		input.StartTime.Format(time.RFC3339),
+		input.EndTime.Format(time.RFC3339),
+		input.Status,
+		input.Venue,
+		input.Organizers,
+		input.BookingLink,
+		input.Source,
+		input.Verified,
+		input.IsPublic,
+	)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	id, _ := result.LastInsertId()
+	input.ID = int(id)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(input)
+}
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query(`
+	SELECT id, title, company, description, start_time, end_time, status, venue, organizers, booking_link, source, verified, is_public
+	FROM events`)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+
+		var e Event
+		var startStr, endStr string
+
+		rows.Scan(
+			&e.ID,
+			&e.Title,
+			&e.Company,
+			&e.Description,
+			&startStr,
+			&endStr,
+			&e.Status,
+			&e.Venue,
+			&e.Organizers,
+			&e.BookingLink,
+			&e.Source,
+			&e.Verified,
+			&e.IsPublic,
+		)
+
+		e.StartTime, _ = time.Parse(time.RFC3339, startStr)
+		e.EndTime, _ = time.Parse(time.RFC3339, endStr)
+
+		events = append(events, e)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(events)
+}
+func getEvent(w http.ResponseWriter, r *http.Request) {
+
+	parts := strings.Split(r.URL.Path, "/")
+	idStr := parts[len(parts)-1]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var e Event
+	var startStr, endStr string
+
+	err = db.QueryRow(`
+	SELECT id, title, company, description, start_time, end_time, status, venue, organizers, booking_link, source, verified, is_public
+	FROM events WHERE id = ?`, id).
+		Scan(
+			&e.ID,
+			&e.Title,
+			&e.Company,
+			&e.Description,
+			&startStr,
+			&endStr,
+			&e.Status,
+			&e.Venue,
+			&e.Organizers,
+			&e.BookingLink,
+			&e.Source,
+			&e.Verified,
+			&e.IsPublic,
+		)
+
+	if err == sql.ErrNoRows {
+		http.Error(w, "Event not found", http.StatusNotFound)
+		return
+	}
+
+	e.StartTime, _ = time.Parse(time.RFC3339, startStr)
+	e.EndTime, _ = time.Parse(time.RFC3339, endStr)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(e)
+}
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+
+	parts := strings.Split(r.URL.Path, "/")
+	idStr := parts[len(parts)-1]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.Exec("DELETE FROM events WHERE id = ?", id)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Event deleted"))
+}
 // Create table with all necessary fields
 func createTable() {
 	query := `
@@ -89,7 +264,7 @@ func createTable() {
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*") // allow any origin
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
@@ -97,150 +272,4 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		}
 		next(w, r)
 	}
-}
-
-// POST /api/v1/events
-func handleEvent(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		getEvent(w, r)
-		return
-	} else if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var input Event
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	// Insert into database
-	result, err := db.Exec(`
-		INSERT INTO events
-		(title, company, description, start_time, end_time, status, venue, organizers, booking_link, source, verified, is_public)
-		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-		input.Title,
-		input.Company,
-		input.Description,
-		input.StartTime.Format(time.RFC3339),
-		input.EndTime.Format(time.RFC3339),
-		input.Status,
-		input.Venue,
-		input.Organizers,
-		input.BookingLink,
-		input.Source,
-		input.Verified,
-		input.IsPublic,
-	)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	id, _ := result.LastInsertId()
-	input.ID = int(id)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(input)
-}
-
-// GET /api/v1/events/{id}
-func getEvent(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	parts := strings.Split(r.URL.Path, "/")
-	idStr := parts[len(parts)-1]
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	var e Event
-	var startStr, endStr string
-
-	err = db.QueryRow(`
-		SELECT id, title, company, description, start_time, end_time, status, venue, organizers, booking_link, source, verified, is_public
-		FROM events
-		WHERE id = ?`, id).Scan(
-		&e.ID,
-		&e.Title,
-		&e.Company,
-		&e.Description,
-		&startStr,
-		&endStr,
-		&e.Status,
-		&e.Venue,
-		&e.Organizers,
-		&e.BookingLink,
-		&e.Source,
-		&e.Verified,
-		&e.IsPublic,
-	)
-	if err == sql.ErrNoRows {
-		http.Error(w, "Event not found", http.StatusNotFound)
-		return
-	} else if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	e.StartTime, _ = time.Parse(time.RFC3339, startStr)
-	e.EndTime, _ = time.Parse(time.RFC3339, endStr)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(e)
-}
-
-// GET /api/v1/events
-func handleEvents(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	rows, err := db.Query(`
-	SELECT id, title, description, start_time, end_time, status
-	FROM events`)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	defer rows.Close()
-
-	var events []Event
-	var startStr, endStr string
-
-	for rows.Next() {
-		var e Event
-
-		err := rows.Scan(
-			&e.ID,
-			&e.Title,
-			&e.Description,
-			&startStr,
-			&endStr,
-			&e.Status,
-		)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		e.StartTime, _ = time.Parse(time.RFC3339, startStr)
-		e.EndTime, _ = time.Parse(time.RFC3339, endStr)
-
-		events = append(events, e)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(events)
 }
